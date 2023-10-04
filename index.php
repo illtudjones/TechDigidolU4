f29643b4c9df742ea05b01a27e7cd32e20be9323
illtudjones <61548275+illtudjones@users.noreply.github.com>
2023-10-04 14:38:48 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -13,9 +13,9 @@
 	</head>
 	<body>
 		<!-- navbar -->
-		<div class = "container-fluid">
-			<!-- first child -->
-			<nav class="navbar navbar-expand-lg bg-body-tertiary">
+		<div class="container-fluid">
+		<!-- first child -->
+		<nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,24 +24,26 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">Hafan</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+          <a class="nav-link" href="#">Register</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
+            Cynhyrchion
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Cyfrifiadur</a></li>
+            <li><a class="dropdown-item" href="#">Glinfwrdd</a></li>
+            <li><a class="dropdown-item" href="#">Mac</a></li>
           </ul>
         </li>
         <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          <a class="nav-link" href="#">Cyswllt</a>
+        </li>
+		<li class="nav-item">
+          <a class="nav-link" href="#">Basged <i class="fa-solid fa-cart-shopping"><sup> 1</sup></i></a>
         </li>
       </ul>
       <form class="d-flex" role="search">
@@ -51,6 +53,7 @@
     </div>
   </div>
 </nav>
+		</div>
 	</body>
 
 
