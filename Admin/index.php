5569bdcd8f2beea5d098fd93ceca3575113c3919
illtudjones <61548275+illtudjones@users.noreply.github.com>
2023-11-19 19:53:57 +0000

    Category/Brand files
    adding category and brand php files and passing to admin index

--- Admin/index.php
+++ Admin/index.php
@@ -9,6 +9,8 @@
     <title>Admin Dashboard</title>
     <!-- Bootstrap CSS Link -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- font awesome link -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     
 </head>
@@ -44,9 +46,9 @@
                 <div class="button text-center">
                     <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">Insert Products</a></button>
                     <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">View Products</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">Insert Categories</a></button>
+                    <button class="my-3"><a href="index.php?insert_categories" class="nav-link text-dark bg-info my-1 mx-1">Insert Categories</a></button>
                     <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">View Categories</a></button>
-                    <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">Insert Brands</a></button>
+                    <button class="my-3"><a href="index.php?insert_brands" class="nav-link text-dark bg-info my-1 mx-1">Insert Brands</a></button>
                     <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">View Brands</a></button>
                     <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">All Orders</a></button>
                     <button class="my-3"><a href="" class="nav-link text-dark bg-info my-1 mx-1">All Payments</a></button>
@@ -57,11 +59,23 @@
             </div>
         </div>
 
+        <!-- fourth child -->
+        <div class="container my-5">
+            <?php
+            if(isset($_GET['insert_categories'])){
+                include('insert_categories.php');
+            }
+            if(isset($_GET['insert_brands'])){
+                include('insert_brands.php');
+            }
+            ?>
+        </div>
+
           <!-- last child-->
         <div class="bg-info p-3 text-center footer">
             <p>All Rights Reserved - Designed by I. Rh. I. Jones - 2023</p>
         </div>
-        
+
     </div>
     
     <!-- boostrap js link -->
