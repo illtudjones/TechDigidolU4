e61084dd9c9314eee9bf5d9303b3dfc9fcfa0f91
illtudjones <61548275+illtudjones@users.noreply.github.com>
2023-11-20 11:03:31 +0000

    Barnds MYSQL
    updating insert brands to update mysql databse

--- Admin/insert_brands.php
+++ Admin/insert_brands.php
@@ -1,10 +1,37 @@
+<?php
+    include('../Includes/connect.php');
+    if(isset($_POST['insert_brand'])){
+        $brand_title=$_POST['brand_title'];
+
+        //check if data already in database
+
+        $select_query = "Select * from `brands` where brand_title='$brand_title'";
+        $result_select = mysqli_query($conn, $select_query);
+        $number = mysqli_num_rows($result_select);
+        if($number>0){
+            echo "<script>alert('This brand is already present')</script>";
+        }else{
+
+            $insert_query="insert into `brands` (brand_title) values ('$brand_title')";
+            $result = mysqli_query($conn,$insert_query);
+            if($result){
+                echo"<script>alert('Brand has been inserted successfully')</script>";
+            }else{
+                echo "Error: " . mysqli_error($conn);
+            }
+    }
+}
+
+?>
+
+
 <form action="" method="post" class="mb-2">
 <div class="input-group w-90 mb-3">
     <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-        <input type="text" class="form-control" name="cat_title" id="cat_title" placeholder="Insert Brand" aria-label="brands" aria-describedby="basic-addon1">
+        <input type="text" class="form-control" name="brand_title" id="brand_title" placeholder="Insert Brand" aria-label="brands" aria-describedby="basic-addon1">
     </div>
     <div class="input-group w-10 mb-2 m-auto">
-        <button class="bg-info p-2 my-3 border-0">Insert Brand</button>    
+    <button class="bg-info p-2 my-3 border-0" name="insert_brand" value="Insert Brand">Insert Brand</button>    
     </div>
     </div>
 </form>
