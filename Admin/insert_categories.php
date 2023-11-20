3d724572cce54f67f323c7ea82f3687b93adce5e
illtudjones <61548275+illtudjones@users.noreply.github.com>
2023-11-20 10:35:41 +0000

    Connection to MYSQL
    fixing connection to mysql
    inserting into database
    checking for duplicates

--- Admin/insert_categories.php
+++ Admin/insert_categories.php
@@ -1,3 +1,29 @@
+<?php
+    include('../Includes/connect.php');
+    if(isset($_POST['insert_cat'])){
+        $category_title=$_POST['cat_title'];
+
+        //check if data already in database
+
+        $select_query = "Select * from `categories` where category_title='$category_title'";
+        $result_select = mysqli_query($conn, $select_query);
+        $number = mysqli_num_rows($result_select);
+        if($number>0){
+            echo "<script>alert('This category is already present')</script>";
+        }else{
+
+            $insert_query="insert into `categories` (category_title) values ('$category_title')";
+            $result = mysqli_query($conn,$insert_query);
+            if($result){
+                echo"<script>alert('Category has been inserted successfully')</script>";
+            }else{
+                echo "Error: " . mysqli_error($conn);
+            }
+    }
+}
+
+?>
+
 <form action="" method="post" class="mb-2">
 <div class="input-group w-90 mb-3">
     <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
@@ -5,7 +31,7 @@
     </div>
     <div class="input-group w-10 mb-2 m-auto">
             <!--input type="submit" class="form-control" name="insert_cat" id="insert_cat" value="Insert Category"-->
-        <button class="bg-info p-2 my-3 border-0">Insert Category</button>    
+        <button class="bg-info p-2 my-3 border-0" name="insert_cat" value="Insert Categories">Insert Category</button>    
     </div>
     </div>
 </form>
--- Includes/connect.php
+++ Includes/connect.php
@@ -1,10 +1,9 @@
 <?php
 // Create connection
-$conn = new mysqli("localhost", "root", "root");
+$conn = new mysqli("localhost", "root", "root", "My_store");
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-echo "Connected successfully";
 ?>
