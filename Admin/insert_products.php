8d6372245d3293f8389bb1623ed9f59f4b0177be
illtudjones <61548275+illtudjones@users.noreply.github.com>
2023-11-20 14:02:11 +0000

    insert product page
    updating with dynamic categories and brands

--- Admin/insert_products.php
+++ Admin/insert_products.php
@@ -1,11 +1,106 @@
+<?php
+    include("../Includes/connect.php")
+?>
+
 <!DOCTYPE html>
+<style>
+<?php include '../style.css'; ?>
+</style>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insert Products - Admin Dashboard</title>
+
+    <!-- Bootstrap CSS Link -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- font awesome link -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
-<body>
+<body class="bg-light">
+    <div class="container mt-3">
+        <h1 class="text-center">Insert Products</h1>
+        <!-- Form -->
+        <form action="" method="post" enctype="multipart/form-data">
+            <!-- title -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_title" class="form-label">Product Title</label>
+                <input type="text" name="product_title" id="product_title" class="form-control" placeholder="Enter Product Title" autocomplete="off" required="required">
+            </div>
+
+            <!-- Description -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_description" class="form-label">Product Description</label>
+                <input type="text" name="product_description" id="product_description" class="form-control" placeholder="Enter Product Description" autocomplete="off" required="required">
+            </div>
+
+            <!-- keywords -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_keywords" class="form-label">Product Keywords</label>
+                <input type="text" name="product_keywords" id="product_keywords" class="form-control" placeholder="Enter Product keywords" autocomplete="off" required="required">
+            </div>
+
+            <!-- categories -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <select name="product_category" id="" class="form-select">
+                    <option value="">Select a Category</option>
+                    <?php
+
+                        $select_query = "Select * from `categories`";
+                        $result_query = mysqli_query($conn, $select_query);
+                        while($row = mysqli_fetch_assoc($result_query)){
+                            $category_title = $row['category_title'];
+                            $category_id = $row['category_id'];
+                            echo "<option value = '$category_id'>$category_title</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+
+            <!-- brands -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <select name="product_brands" id="" class="form-select">
+                    <option value="">Select a Brand</option>
+                  
+                    <?php
+
+                        $select_query = "Select * from `brands`";
+                        $result_query = mysqli_query($conn, $select_query);
+                        while($row = mysqli_fetch_assoc($result_query)){
+                            $brand_title = $row['brand_title'];
+                            $brand_id = $row['brand_id'];
+                            echo "<option value = '$brand_id'>$brand_title</option>";
+                        }
+                    ?>
+                </select>
+            </div>
+
+            <!-- Image1 -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_iamge1" class="form-label">Product Image 1</label>
+                <input type="file" name="product_image1" id="product_image1" class="form-control">
+            </div>
+
+            <!-- Image2 -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_iamge2" class="form-label">Product Image 2</label>
+                <input type="file" name="product_image2" id="product_image2" class="form-control">
+            </div>
+
+            <!-- price -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <label for="product_price" class="form-label">Product Price</label>
+                <input type="text" name="product_price" id="product_price" class="form-control" placeholder="Enter Product Price" autocomplete="off" required="required">
+            </div>
+
+            <!-- price -->
+            <div class="form-outline mb-4 w-50 m-auto">
+                <input type="submit" name="insert_product" id="insert_product" class="btn btn-info mb-3 px-3" value = "Insert Product">
+            </div>
+
+        </form>
+    </div>
     
 </body>
 </html>
